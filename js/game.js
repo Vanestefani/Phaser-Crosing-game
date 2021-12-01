973e326f377b422e7dd0d737f25c7ef362a4d48b
Vanessa Estefania Corredor Andrade <47900659+Vanestefani@users.noreply.github.com>
2021-11-30 21:04:17 -0500

    creando grupo de enemigos y efecto camara

--- js/game.js
+++ js/game.js
@@ -13,6 +13,9 @@ gameScene.init = function () {
   // boundaries
   this.enemyMinY = 80;
   this.enemyMaxY = 280;
+
+  // we are not terminating
+  this.isTerminating = false;
 };
 
 // load assets
@@ -58,21 +61,44 @@ gameScene.create = function () {
   );
   this.goal.setScale(0.6);
 
-  // enemy
-  this.enemy = this.add.sprite(120, this.sys.game.config.height / 2, "enemy");
-  this.enemy.flipX = true;
-  this.enemy.setScale(0.6);
+  // enemy group
+  this.enemies = this.add.group({
+    key: "enemy",
+    repeat: 5,
+    setXY: {
+      x: 90,
+      y: 100,
+      stepX: 80,
+      stepY: 20
+    }
+  });
 
-  // set enemy speed
-  let dir = Math.random() < 0.5 ? 1 : -1;
-  let speed =
-    this.enemyMinSpeed +
-    Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
-  this.enemy.speed = dir * speed;
+  //setting scale to all group elements
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.4, -0.4);
+
+  //set flipX, and speed
+  Phaser.Actions.Call(
+    this.enemies.getChildren(),
+    function (enemy) {
+      //flip enemy
+      enemy.flipX = true;
+
+      //set speed
+      let dir = Math.random() < 0.5 ? 1 : -1;
+      let speed =
+        this.enemyMinSpeed +
+        Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+      enemy.speed = dir * speed;
+    },
+    this
+  );
 };
 
 // this is called up to 60 times per second
 gameScene.update = function () {
+  // don't execute if we are terminating
+  if (this.isTerminating) return;
+
   // check for active input
   if (this.input.activePointer.isDown) {
     // player walks
@@ -86,22 +112,62 @@ gameScene.update = function () {
   if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
     console.log("reached goal!");
 
-    // restart the Scene
-    this.scene.restart();
-    return;
+    return this.gameOver();
   }
 
-  // enemy movement
-  this.enemy.y += this.enemy.speed;
+  //get enemies
+  let enemies = this.enemies.getChildren();
+  let numEnemies = enemies.length;
 
-  // check we haven't passed min or max Y
-  let conditionUp = this.enemy.speed < 0 && this.enemy.y <= this.enemyMinY;
-  let conditionDown = this.enemy.speed > 0 && this.enemy.y >= this.enemyMaxY;
+  for (let i = 0; i < numEnemies; i++) {
+    // enemy movement
+    enemies[i].y += enemies[i].speed;
 
-  // if we passed the upper or lower limit, reverse
-  if (conditionUp || conditionDown) {
-    this.enemy.speed *= -1;
+    // check we haven't passed min or max Y
+    let conditionUp = enemies[i].speed < 0 && enemies[i].y <= this.enemyMinY;
+    let conditionDown = enemies[i].speed > 0 && enemies[i].y >= this.enemyMaxY;
+
+    // if we passed the upper or lower limit, reverse
+    if (conditionUp || conditionDown) {
+      enemies[i].speed *= -1;
+    }
+
+    // check enemy overlap
+    let enemyRect = enemies[i].getBounds();
+
+    if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)) {
+      console.log("Game over!");
+
+      return this.gameOver();
+    }
   }
+};
+
+gameScene.gameOver = function () {
+  // initiated game over sequence
+  this.isTerminating = true;
+
+  // shake camera
+  this.cameras.main.shake(500);
+
+  // listen for event completion
+  this.cameras.main.on(
+    "camerashakecomplete",
+    function (camera, effect) {
+      // fade out
+      this.cameras.main.fade(500);
+    },
+    this
+  );
+
+  this.cameras.main.on(
+    "camerafadeoutcomplete",
+    function (camera, effect) {
+      // restart the Scene
+      this.scene.restart();
+    },
+    this
+  );
 };
 
 // set the configuration of the game
