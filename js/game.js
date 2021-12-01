9bfe3080ff57ff2eafdae79a79cf44814555e8e1
Vanessa Estefania Corredor Andrade <47900659+Vanestefani@users.noreply.github.com>
2021-11-30 20:37:01 -0500

    añadiendo sprites

--- js/game.js
+++ js/game.js
@@ -1,7 +1,28 @@
-// nueva escena
+// create a new scene
 let gameScene = new Phaser.Scene("Game");
 
-// Configuracionde juego
+// load assets
+gameScene.preload = function () {
+  // load images
+  this.load.image("background", "assets/background.png");
+  this.load.image("player", "assets/player.png");
+};
+
+// called once after the preload ends
+gameScene.create = function () {
+  // create bg sprite
+  let bg = this.add.sprite(0, 0, "background");
+
+  // place sprite in the center
+  bg.setPosition(640 / 2, 360 / 2);
+
+  let gameW = this.sys.game.config.width;
+  let gameH = this.sys.game.config.height;
+
+  let player = this.add.sprite(70, 180, "player");
+};
+
+// set the configuration of the game
 let config = {
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
   width: 640,
@@ -9,5 +30,5 @@ let config = {
   scene: gameScene
 };
 
-// crear jeugo con configuracion
+// create a new game, pass the configuration
 let game = new Phaser.Game(config);
